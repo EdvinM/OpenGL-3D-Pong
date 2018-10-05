5d0d7b9d1b6557938845b1947c8c8709526f78d7
= <mako_edvin@yahoo.com>
2018-10-05 17:21:28 +0200

    Added pong source for pong game

--- src/task2_convolution/task2_convolution.cpp
+++ src/task2_convolution/task2_convolution.cpp
@@ -8,10 +8,13 @@
 using namespace std;
 using namespace ppgso;
 
-const int kernel_height = 5;
-const int kernel_width = 5;
+const int kernel_height = 3;
+const int kernel_width = 3;
 float kernel[kernel_height][kernel_width] {
     // TODO: Define the kernel
+        {0, -1, 0},
+        {-1, 5, -1},
+        {0, -1, 0}
 };
 float kernel_factor = 1.0f;
 float kernel_bias = 0.0f;
@@ -36,15 +39,34 @@ int main()
       float r = 0, g = 0, b = 0;
 
       // TODO: Compute convolution
+      for(int k_y = 0; k_y < kernel_height; k_y++) {
+
+          if(y == 0)
+              k_y += 1;
+
+          int cur_pixel_y = (y - (kernel_height / 2) + k_y) % source.height;
+
+        for(int k_x = 0; k_x < kernel_width; k_x++) {
+
+            if(x == 0)
+                k_x += 1;
+
+            int cur_pixel_x = (x - (kernel_width / 2) + k_x) % source.width;
+
+            r = (r + source.getPixel(cur_pixel_x, cur_pixel_y).r * kernel[k_y][k_x]);
+            g = (g + source.getPixel(cur_pixel_x, cur_pixel_y).g * kernel[k_y][k_x]);
+            b = (b + source.getPixel(cur_pixel_x, cur_pixel_y).b * kernel[k_y][k_x]);
+        }
+      }
 
       // Store the result divided by factor
-      destination.setPixel(x, y, r, g, b);
+      destination.setPixel(x, y, r / 256, g / 256, b / 256);
     }
   }
 
   // Save as result
   cout << "Generating task2_convolution.bmp file ..." << endl;
-  image::saveBMP(destination, "task2_convolution.bmp");
+  image::saveBMP(destination, "task3_convolution.bmp");
 
   cout << "Done." << endl;
   return EXIT_SUCCESS;
